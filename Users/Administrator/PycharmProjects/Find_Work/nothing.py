43929ff557f1557badfa2ab6d9484de8c92f3ad3
ZC <812075951@qq.com>
2020-08-21 19:41:22 +0800

    2020年8月21日19:40:48  小改动

---
+++ Users/Administrator/PycharmProjects/Find_Work/nothing.py
@@ -0,0 +1,6 @@
+from zc_test_1 import Solution
+#卧槽  只要导入了其他的py文件，会默认运行里面的print函数，长见识了！
+
+s2=Solution()
+
+print(s2.minFlips("001000101"))
--- Users/Administrator/PycharmProjects/Find_Work/zc_test_1.py
+++ Users/Administrator/PycharmProjects/Find_Work/zc_test_1.py
@@ -1,3 +1,5 @@
+#这是一个，力扣的题目答案，大概是二进制多位数，转换成全是0的最小次数，如100，
+# 转化成000需要反转几次
 class Solution:
     def minFlips(self, target: str) -> int:
         flips, prev = 0, "0"
